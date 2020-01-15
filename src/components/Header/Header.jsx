8b15d7e5c0a271cc0d1a9124ea85613997db1b27
Oleg Bershak <obershak@gmail.com>
2020-01-16 00:21:45 +0300

    Added styles to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  'header': {
+    'width': '100%',
+    'height': 40,
+    'background': '#343538',
+    'font': '700 20px Roboto',
+    'display': 'flex',
+    'align-items': 'center',
+    'justify-content': 'center',
+  },
+  'wrapper': {
+    '@media (min-width: 768px)': {
+      'width': 700,
+    },
+    '@media (min-width: 1024px)': {
+      'width': 960,
+    },
+    'width': 340,
+  },
+
+  'title': {
+    'color': '#feffff',
+  },
+});
 
 function Header() {
+  const style = useStyles();
+
   return (
-    <div className="header">
-      <div className="header__name">
-        Simple Chat
+    <header className={style.header}>
+      <div className={style.wrapper}>
+        <div className={style.title}>
+          Simple Chat
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
